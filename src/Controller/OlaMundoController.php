9a226dafb777daa8488a2e4c69d34fc09a0db6b0
edualvesz <eduardo_ean@outlook.com>
2022-08-19 15:21:23 -0300

    reading require's data

--- src/Controller/OlaMundoController.php
+++ src/Controller/OlaMundoController.php
@@ -1,6 +1,10 @@
 <?php 
 
 namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class OlaMundoController
@@ -8,9 +12,14 @@ class OlaMundoController
     /** 
      *  @Route ("/ola")
      */
-    public function olaMundoAction(){
-        echo 'Ola mundo!';
-        exit();
+    public function olaMundoAction(Request $request):Response{
+        $pathInfo = $request->getPathInfo();
+        $query = $request->query->all(); //all returns associative array
+        return new JsonResponse([
+            'mensagem' => 'Ola Mundo!', 
+            'pathinfo' => $pathInfo,
+            'query' => $query
+        ]);
     }
 }
 
